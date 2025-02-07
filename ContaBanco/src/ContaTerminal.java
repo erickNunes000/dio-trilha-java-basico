c2d1d5f410af31ff592c6e6026204921fd28963b
Erick Nunes <erickFernandes000@gmail.com>
2025-02-07 20:16:49 -0300

    text altered

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -19,7 +19,7 @@ public class ContaTerminal {
         System.out.println("Por favor, digite a Agência !");
         String agencia = sc.next();
         
-        String msg = "Olá "+ nomeCliente + " , obrigado por criar uma conta em nosso banco, sua agência é"+ agencia+ ", conta "+numero+ "e seu saldo "+saldo+ " já está disponível para saque.";
+        String msg = "Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é"+ agencia+ ", conta "+numero+ " e seu saldo "+saldo+ " já está disponível para saque.";
         
         System.out.println(msg);
     }
